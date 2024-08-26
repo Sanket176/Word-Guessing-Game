21d0f428de4551deaa5cdd2b1c725095b06308a6
Sanket176 <sanketrinayat17@gmail.com>
2024-08-26 18:03:40 +0530

    Hint Compont UI added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { useState } from 'react'
-import Input from './Components/input'
-import Text from  './Components/text'
 import './App.css'
-import Tag from './Components/tag'
-import Button from './Components/button'
+import Hint from './Hint'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,23 +8,17 @@ function App() {
   return (
     <>
       <h1>Word Guessing Game</h1>
-      <Tag label="score"/>
+      <Hint />
+
+      {/* <Tag label="score"/>
       <Input /> &nbsp;
       <Input /> &nbsp;
       <Input /> &nbsp;
       <Input /> &nbsp;
       <Text text="Apple" type={Text.StyleType.ANSWER_HINT}/>
       <Button type={Button.ButtonType.CIRCULAR} label='prev' onClick={console.log("prev btn pressed")}/>
-      <Button type={Button.ButtonType.KEYBOARD} label='S' onClick={console.log("S btn pressed")}/>
+      <Button type={Button.ButtonType.KEYBOARD} label='S' onClick={console.log("S btn pressed")}/> */}
 
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
     </>
   )
 }
